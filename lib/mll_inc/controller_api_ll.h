4874f3ef1f3be380cab08c1f931b547d945f26f9
Micr_SW Connectivity_01 <micr_sw.connectivity_01@nxp.com>
2025-02-20 16:23:14 +0100

    [MCU] Update linklayer libraries and nbu from private_repository:be3a6464fcd3581e6e5d525cdba07123aa030718

--- lib/mll_inc/controller_api_ll.h
+++ lib/mll_inc/controller_api_ll.h
@@ -1,25 +1,15 @@
-/*                Copyright 2021-2022, NXP
- *
- *  NXPCONFIDENTIAL
- *  The source code contained or described herein and all documents related to
- *  the source code ( Material ) are owned by NXP its
- *  suppliers or licensors. Title to the Material remains with NXP
- *  or its suppliers and licensors. The Material contains
- *  trade secrets and proprietary and confidential information of NXP its
- *  suppliers and licensors. The Material is protected by worldwide copyright
- *  and trade secret laws and treaty provisions. No part of the Material may be
- *  used, copied, reproduced, modified, published, uploaded, posted,
- *  transmitted, distributed, or disclosed in any way without NXPprior
- *  express written permission.
- *
- *  No license under any patent, copyright, trade secret or other intellectual
- *  property right is granted to or conferred upon you by disclosure or delivery
- *  of the Materials, either expressly, by implication, inducement, estoppel or
- *  otherwise. Any license under such intellectual property rights must be
- *  express and approved by NXP writing.
- */
- 
-/* WARNING: DO NOT MODIFY THIS FILE directly as this file is opied from LL. */
+/*! *********************************************************************************
+*
+* Copyright 2021-2025 NXP
+*
+* NXP Proprietary. 
+* This software is owned or controlled by NXP and may only be used strictly in accordance 
+* with the applicable license terms.  By expressly accepting such terms or by downloading, 
+* installing, activating and/or otherwise using the software, you are agreeing that you 
+* have read, and that you agree to comply with and are bound by, such license terms.  
+* If you do not agree to be bound by the applicable license terms, then you may not retain, 
+* install, activate or otherwise use the software.
+********************************************************************************** */
 
 #ifndef CONTROLLER_API_H_
 #define CONTROLLER_API_H_
