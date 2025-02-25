f633f7e8639385e3a2fef956b8aceae28237eb8d
Micr_SW Connectivity_01 <micr_sw.connectivity_01@nxp.com>
2025-02-25 15:09:30 +0100

    [MCU] Update linklayer libraries and nbu from private_repository:5ab2d9faeb9a72e8ff0b6c653527ce1c88682b85

--- lib/mll_inc/ble_hadm_hal.h
+++ lib/mll_inc/ble_hadm_hal.h
@@ -1,14 +1,15 @@
-/*
- * Copyright 2021-2022 NXP
- * All rights reserved.
- * SPDX-License-Identifier: BSD-3-Clause
- */
-/*
- * This file is an abstract layer API that must be implemented by each
- * hardware platform.
- * Be carefull not using any environement-specific types such as uint8_t or uint8
- * as this file is shared bw C&S LL code and SDK code.
- */
+/*! *********************************************************************************
+*
+* Copyright 2021-2025 NXP
+*
+* NXP Proprietary. 
+* This software is owned or controlled by NXP and may only be used strictly in accordance 
+* with the applicable license terms.  By expressly accepting such terms or by downloading, 
+* installing, activating and/or otherwise using the software, you are agreeing that you 
+* have read, and that you agree to comply with and are bound by, such license terms.  
+* If you do not agree to be bound by the applicable license terms, then you may not retain, 
+* install, activate or otherwise use the software.
+********************************************************************************** */
 
 /* Prevent double inclusion */
 #ifndef _BLE_HADM_HAL_H_
