0dec3877ab76bca86c6ff4486b212b2825c49af3
Corentin Goubin <corentin.goubin@nxp.com>
2025-02-26 11:12:44 +0100

    [MCSE-678] Remove T1_INT_IRQHandler from nbu_ble_wrap.s as it is now defined in framework

--- src/KW4x/armgcc/nbu_ble_wrap.s
+++ src/KW4x/armgcc/nbu_ble_wrap.s
@@ -34,7 +34,6 @@
   .global     BTU_FIQ_IRQHandler
   .global     BTU_INT_IRQHandler
   .global     CTI_IRQ0_IRQHandler
-  .global     T1_INT_IRQHandler
   .global     T2_INT_IRQHandler
   .global     T3_INT_IRQHandler
   .global     T4_INT_IRQHandler
@@ -75,7 +74,6 @@ BLE_INT2_IRQHandler:
 BTU_FIQ_IRQHandler:
 BTU_INT_IRQHandler:
 CTI_IRQ0_IRQHandler:
-T1_INT_IRQHandler:
 T2_INT_IRQHandler:
 T3_INT_IRQHandler:
 T4_INT_IRQHandler:
