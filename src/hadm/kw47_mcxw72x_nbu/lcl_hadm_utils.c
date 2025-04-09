9823905444823e52c3ed6070d9964c4691c03a2f
Tri Nguyen <tri.nguyen@nxp.com>
2025-04-09 10:08:53 +0200

    [LL-1905][HADM] ACI0 - assigns the antenna gpio for all four entries to the 1:1 antenna path

--- src/hadm/kw47_mcxw72x_nbu/lcl_hadm_utils.c
+++ src/hadm/kw47_mcxw72x_nbu/lcl_hadm_utils.c
@@ -386,6 +386,16 @@ void lcl_hadm_utils_configure_antenna_switching(hadm_meas_t *hadm_meas_p)
      * In these configurations, antenna path A1 is assigned to the 1:1 antenna combination, A2 is assigned to the 1:2 or 2:1 combination, 
      * A3 is assigned to the 1:3 or 3:1 combination and A4 is assigned to the 1:4 or 4:1 combination. 
      * The exception is the 2:2 configuration where A1 is assigned to 1:1, A2 is assigned to 1:2, A3 is assigned to 2:1 and A4 is assigned to 2:2. " */
+    if (hadm_meas_p->config_p->toneAntennaConfigIdx == HADM_ANT_CFG_IDX_0)
+    {
+        uint8_t ant_gpio[HADM_MAX_NB_ANTENNA_PATHS];
+        ant_gpio[0] = hadm_device.ant2gpio[0];
+        ant_gpio[1] = hadm_device.ant2gpio[0];
+        ant_gpio[2] = hadm_device.ant2gpio[0];
+        ant_gpio[3] = hadm_device.ant2gpio[0];
+        status = XCVR_LCL_ConfigLclBlock(&hadm_meas_p->rsm_config, (XCVR_RSM_T_CAPTURE_SEL_T)hadm_meas_p->config_p->T_PM_Time, ant_gpio, ena_antsw_pa_ramping);
+    }
+    else
     if (hadm_meas_p->config_p->toneAntennaConfigIdx == HADM_ANT_CFG_IDX_7)
     {
         uint8_t ant_gpio[HADM_MAX_NB_ANTENNA_PATHS];
