fb57f3232629119e8a3c3b98900b1267c170eeab
Micr_SW Connectivity_01 <micr_sw.connectivity_01@nxp.com>
2026-01-19 11:45:05 +0100

    [MCU] Update linklayer libraries and nbu from private_repository:9e812a6db561fa8e10bf1da0c9d2fd41375c7a85

--- lib/mll_inc/ble_hadm_api.h
+++ lib/mll_inc/ble_hadm_api.h
@@ -24,6 +24,10 @@
 
 #include "ble_hadm_types.h"
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 /* === Variables  =========================================================== */
 
 
@@ -52,5 +56,9 @@ BLE_HADM_STATUS_t BLE_HADM_SubeventContinue(BLE_HADM_SubeventConfig_t *pConfig, 
  * \return    HADM_HAL_SUCCESS indicates LL continuing the procedure, HADM_HAL_ABORTED otherwise 
  */
 BLE_HADM_STATUS_t BLE_HADM_ProcedureContinue(const TBleHadmConnection_t *pProc, uint32 startTimeHSlot, uint32 startTimeOffsetUs);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif // #ifndef __HADM_API_H_
