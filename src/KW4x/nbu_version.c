6fdf2ab0e70ea4be44fa3b5f85e44516bba3f509
Olivier Balitch <olivier.balitch@nxp.com>
2026-06-19 15:19:56 +0000

    [LL-3178] Update NBU version 1.9.36.0 for release 26.09.00-pvw1

--- src/KW4x/nbu_version.c
+++ src/KW4x/nbu_version.c
@@ -15,7 +15,7 @@
 /*! \cond DOXY_SKIP_TAG */
 #define gNbuVerMajor_c   1
 #define gNbuVerMinor_c   9
-#define gNbuVerPatch_c   35
+#define gNbuVerPatch_c   36
 #define gNbuVerBuildNo_c 0
 #define gNbuModuleId_c   0x11
 /*! \endcond */
